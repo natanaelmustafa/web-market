8778ff5e99ea6dfbf53e3fccbae1d82a46e9fc24
NATANAEL <natanaelmustafa@gmail.com>
2024-02-26 19:48:10 -0300

    second commit

--- web_market/src/components/Footer.tsx
+++ web_market/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Logo from '../assets/imgs/logo_.png'
 const Footer = () =>{
     return(
         <div className='footer-container'>
-            <div className="footer">
+            <div className="footer d-flex align-items-center">
                 <span>Super mercado Super Barato. © 2024 Todos os direitos reservados</span>
                 <span className='brand-text'><img className='logo-footer' src={Logo} alt="" /> Super Barato</span>
             </div>
